824bdad3067a9add10fa5dd05de53624c3c7825f
Brandon Chase <brandonjameschase@gmail.com>
2017-09-10 20:21:41 -0500

    Progress

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,26 @@ Sources:
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h> //getcwd()
+#include <unistd.h>
 #include <dirent.h>
 #include <string.h>
 #include <time.h>
 
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
+#define MAX_NUMBER_OF_FILES_DIRECTORIES 1024
 #define MAX_FILENAME_LENGTH 2048
 #define MAX_PATH_LENGTH 4096
+#define FRAME_SCROLL_LENGTH 10 //number of files/directories to display in one print window
 
 using namespace std;
 
 typedef struct
 {
 	char name[MAX_FILENAME_LENGTH];
-	int size;
+	size_t size;
 	time_t time_last_modified;
 } File;
 
@@ -79,6 +83,7 @@ File generateFile(char* directory_path, char* file_name)
 
 	//construct absolute path name for desired file
 	char full_file_path[MAX_PATH_LENGTH+MAX_FILENAME_LENGTH];
+	strcpy(full_file_path, "");//resets variable, somehow stayed the same since last function call despite going out of scope
 	strcat(full_file_path, directory_path);
 	strcat(full_file_path, "/");
 	strcat(full_file_path, file_name);
@@ -93,18 +98,19 @@ File generateFile(char* directory_path, char* file_name)
 	return result_file;
 }
 
-void processFiles(vector<File>& files, char* directory_path)
+void processFiles(vector<File>& files, char* directory_path, int& number_of_entities)
 {
 	DIR* current_directory = opendir(".");
 	if(current_directory != NULL)
 	{
 		struct dirent* current_dirent;
-		while((current_dirent = readdir(current_directory)))
+		while((current_dirent = readdir(current_directory)) && number_of_entities <= MAX_NUMBER_OF_FILES_DIRECTORIES)
 		{
 			if((current_dirent->d_type) & DT_REG) //if dirent entity is a directory
-		{
+			{
 				File temp_file = generateFile(directory_path, current_dirent->d_name);
 				files.push_back(temp_file);
+				number_of_entities++;
 			}
 		}
 		closedir(current_directory);
@@ -116,19 +122,20 @@ void processFiles(vector<File>& files, char* directory_path)
 	}
 }
 
-void processDirectories(vector<Directory>& directories)
+void processDirectories(vector<Directory>& directories, int& number_of_entities)
 {
 	DIR* current_directory = opendir(".");
 	if(current_directory != NULL)
 	{
 		struct dirent* current_dirent;
-		while ((current_dirent = readdir(current_directory)))
+		while ((current_dirent = readdir(current_directory)) && number_of_entities <= MAX_NUMBER_OF_FILES_DIRECTORIES)
 		{
 			if((current_dirent->d_type) & DT_DIR) //if dirent entity is a directory
 			{
 				Directory temp_directory;
 				strcpy(temp_directory.name, current_dirent->d_name);
 				directories.push_back(temp_directory);
+				number_of_entities++;
 			}
 		}
 
@@ -159,7 +166,7 @@ void printFiles(const vector<File>& files)
 	int i = 0;
 	for(File f : files)
 	{
-		printf("\t%d. %s\n", i++, f.name);
+		printf("\t%d. %s ~ %dB ~ %s\n", i++, f.name, f.size, ctime(&f.time_last_modified));
 	}
 
 	printf("\n");
@@ -168,12 +175,34 @@ void printFiles(const vector<File>& files)
 void printOperations()
 {
 	printf("Operations:\n");
-	printf("\te   Edit\n");
-	printf("\tr   Run\n");
-	printf("\tc   Change DIrectory\n");
-	printf("\ts   Sort Files\n");
-	printf("\tq   Quit\n");
+	printf("\te     Edit\n");
+	printf("\tr     Run\n");
+	printf("\tc     Change Directory\n");
+	printf("\ts     Sort Files\n");
+	printf("\tp/n   Scroll Files\n");
+	printf("\tP/N   Scroll Directories\n");
+	printf("\tq     Quit\n");
 
+}
+
+bool compareBySize(const File& a, const File& b)
+{
+	return a.size > b.size; //we want descending size
+}
+
+void sortFilesBySize(vector<File>& files)
+{
+	sort(files.begin(), files.end(), compareBySize);
+}
+
+bool compareByDate(const File& a, const File& b)
+{
+	return a.time_last_modified > b.time_last_modified; //we want descending time
+}
+
+void sortFilesByDate(vector<File>& files)
+{
+	sort(files.begin(), files.end(), compareByDate);
 }
 
 int main(int argc, char *argv[])
@@ -184,6 +213,9 @@ int main(int argc, char *argv[])
 		changeDirectory(argv[1]);
 	}
 	
+
+	bool sortingBySize = false;
+	bool sortingByDate = false;
 	while (1)
 	{
 		system("clear"); //reset terminal display
@@ -196,23 +228,37 @@ int main(int argc, char *argv[])
 
 		printDate();
 
-		//----------Process FIlesystem Data----------
-
+		//----------Process Filesystem Data----------
+		int number_of_entities = 0;
 		vector<Directory> directories;
-		processDirectories(directories);
+		processDirectories(directories, number_of_entities);
 
 		vector<File> files;
-		processFiles(files, directory_path);
+		processFiles(files, directory_path, number_of_entities);
+
+		//since building files list from scratch on every loop, need to resort new list if user wants sorted
+		if(sortingBySize)
+		{
+			sortFilesBySize(files);
+		}
+
+		else if(sortingByDate)
+		{
+			sortFilesByDate(files);
+		}
 
 		printDirectories(directories);
 
 		printFiles(files);
+
+		printOperations();
 
 		//----------Perform Operations----------
 		printf("-------------------------------------------\n\n");
 
 		char c;
 		char command[256+MAX_FILENAME_LENGTH], input[MAX_FILENAME_LENGTH];
+		pid_t child;
 
 		scanf(" %c", &c);
 		switch(c)
@@ -223,19 +269,52 @@ int main(int argc, char *argv[])
 				printf("Edit what?: ");
 				scanf(" %s", input);
 				strcpy(command, "pico ");
-				strcat(command, s);
+				strcat(command, input);
 				system(command);
 				break;
 			case 'r':
 				printf("Run what?: ");
-				scanf(" %s", command);
+				scanf(" %s", input);
+				printf("\n");
+				strcpy(command, "./");
+				strcat(command, input);
 				system(command);
-				break;*/
+				printf("\nEnter any character to continue.\n");
+				cin >> ws; //ignore trailing newline
+				getchar();
+				break;
 			case 'c':
 				printf("Change to which directory?: ");
 				scanf("%s", input);
 				changeDirectory(input);
+				//don't want to immediately sort files when moving to another directory
+				sortingBySize = false;
+				sortingByDate = false;
 				break;
+			case 's':
+				printf("Sort by size (s) or date last modified(d)?: ");
+				scanf(" %c", &c);
+				if(c == 's') 
+				{
+					sortingBySize = true;
+					sortingByDate = false;
+				}
+
+				else if(c == 'd')
+				{
+					sortingByDate = true;
+					sortingBySize = false;
+				}
+
+				else
+				{
+					printf("***ERROR: \'%c\' is invalid!\n");
+					printf("\nEnter any character to continue.\n");
+					cin >> ws; //ignore trailing newline
+					getchar();
+				}
+				break;
+
 		}
 	}
 }
